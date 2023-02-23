158c141a3957e0d79dd88ef57ab36bf779c02eeb
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 12:24:36 -0800

    Create Q9.ts

---
+++ Assignment-Q9/Q9.ts
@@ -0,0 +1,7 @@
+/*Store your favorite number in a variable. 
+Then, using that variable, 
+create a message that reveals your favorite number.
+*/
+
+let FavouriteNumber = 6
+console.log(`My favourite number is ${FavouriteNumber}`);
