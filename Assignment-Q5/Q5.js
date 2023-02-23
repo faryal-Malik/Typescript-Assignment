cfe3ffbe9e348b00f737d2e5d3d14d7a8cf1bb63
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 11:50:19 -0800

    Create Q5.js

---
+++ Assignment-Q5/Q5.js
@@ -0,0 +1,5 @@
+/*Famous_Person = Albert Einstien
+Qoute = A person who never made a mistake never tried anything new
+*/
+var Famous_person = 'Albert Einstien';
+console.log("".concat(Famous_person, " once said,\"A person who never made a mistake never tried anything new.\""));
