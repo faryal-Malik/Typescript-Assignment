42d371f609fdc9041a9807715b95bc0fc95185ba
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 11:26:02 -0800

    Create Q4.js

---
+++ Assignment-Q4/Q4.js
@@ -0,0 +1,8 @@
+/*Favourite Qoute
+    "A person who never made a mistake never tried anything new"
+author
+     Albert Einstein
+*/
+var author = 'Albert Einstein';
+var famousQoute = 'A person who never made a mistake never tried anything new';
+console.log("".concat(author, " once said, \"").concat(famousQoute, "\""));
