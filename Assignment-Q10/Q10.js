14959171ad2912cc205a3f21f6c83b7b9ad20b46
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 12:35:18 -0800

    Create Q10.js

---
+++ Assignment-Q10/Q10.js
@@ -0,0 +1,12 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.
+*/
+//Name = Faryal Malik
+//Date = 23/02/2023
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+//In this program we did mathematical operations like addition, subtraction, multiplication and division and print answer.
