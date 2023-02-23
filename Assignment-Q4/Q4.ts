6252b6181252c505faa4e22c42e7a34b28d439c5
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 11:26:59 -0800

    Create Q4.ts

---
+++ Assignment-Q4/Q4.ts
@@ -0,0 +1,9 @@
+/*Favourite Qoute
+    "A person who never made a mistake never tried anything new"
+author
+     Albert Einstein
+*/
+
+let author = 'Albert Einstein';
+let famousQoute = 'A person who never made a mistake never tried anything new';
+console.log(`${author} once said, "${famousQoute}"`);
