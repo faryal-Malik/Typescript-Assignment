c7a5f0777a1e2e2700cd27b0bfc6013e712202d4
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 12:24:41 -0800

    Create Q9.js

---
+++ Assignment-Q9/Q9.js
@@ -0,0 +1,6 @@
+/*Store your favorite number in a variable.
+Then, using that variable,
+create a message that reveals your favorite number.
+*/
+var FavouriteNumber = 6;
+console.log("My favourite number is ".concat(FavouriteNumber));
