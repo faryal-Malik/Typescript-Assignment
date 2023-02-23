ffcb466a132621d9291bfe47f4ef35148c2b70b9
faryal-Malik <125805090+faryal-Malik@users.noreply.github.com>
2023-02-23 12:06:27 -0800

    Create Q5.ts

---
+++ Assignment-Q5/Q5.ts
@@ -0,0 +1,6 @@
+/*Famous_Person = Albert Einstien
+Qoute = A person who never made a mistake never tried anything new
+*/
+
+let Famous_person = 'Albert Einstien';
+console.log(`${Famous_person} once said,"A person who never made a mistake never tried anything new."`);
